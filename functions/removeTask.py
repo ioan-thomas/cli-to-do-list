01fdc37f2e5f1e6d5222c0f72ec53dbc73a23d4d
Ioan Thomas <ioanthomas21@gmail.com>
2022-12-19 17:41:14 +0000

    feat: handle user exit

--- functions/removeTask.py
+++ functions/removeTask.py
@@ -5,16 +5,28 @@ def removeTask(conn, cursor):
 
     while True:
         try:
-            # asks user for ID, or 
+            # asks user for ID, if user enters "TASKS" (or anything else other than an ID) a valueError will be thrown (see below).
             taskID = int(input("Enter the ID of the task that you wish to remove or type TASKS to list all tasks: "))
+
+            # the SQL query to be ran
             query = """UPDATE tasks SET Removed = TRUE WHERE TaskID = ?"""
             
+            #executing the SQL query above with the task as the parameter (in place of the "?").
             cursor.execute(query, (taskID,))
 
+        # if the user input results in anything other than an integer (a taskID), then the user will be shown the list of tasks
         except ValueError:
             viewTasks(conn, cursor)
+        
+        # if user exits app, they are greeted with the goodbye message
+        except KeyboardInterrupt:
+            print("\n\nGoodbye")
+            return
+
+        # if an error occurs, the user will be presented with the below error text.
         except:
             print('There was an error removing that task. Please check the taskID and try again.')
+
 
         else:
             conn.commit()
