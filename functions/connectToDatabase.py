112079e92999fac506438c12e9e66ebaedfc2118
Ioan Thomas <ioanthomas21@gmail.com>
2022-12-19 12:53:40 +0000

    !feat: connectToDatabase function now in own file

--- functions/connectToDatabase.py
+++ functions/connectToDatabase.py
@@ -1,1 +1,14 @@
+import sqlite3
 
+# connecting to the database / creating one if it does not exist.
+
+# this function allows for any arugment to be supplied (databasePath) so the app to be expanded in future if need be.
+
+def connectToDatabase(databasePath):
+    conn = sqlite3.connect(databasePath)
+    cursor = conn.cursor()
+
+    return (conn, cursor)
+
+
+# can you make two of the same name?
