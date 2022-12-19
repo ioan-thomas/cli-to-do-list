95bbeab696298156e62fe14984f6b4e06e78f6a3
Ioan Thomas <ioanthomas21@gmail.com>
2022-12-19 12:47:33 +0000

    !feat: createTable function now in own file

---
+++ functions/createTable.py
@@ -0,0 +1,27 @@
+from connectToDatabase import connectToDatabase
+
+def createTable():
+    # this function creates a table in the database if it doesn't already exist
+
+    # retrieving the connection and cursor object to interact with the database.
+    db = connectToDatabase('../tasks.db')
+    conn = db[0]
+    cursor = db[1]
+
+    # the SQL query to be run 
+    query = """CREATE TABLE tasks
+    (TaskID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Title TEXT NOT NULL,
+    Details TEXT NOT NULL,
+    Dateadded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Removed BOOLEAN NOT NULL DEFAULT FALSE)"""
+
+    # attempts to create the table using the above query but shows a message if one already exists
+    try: 
+        cursor.execute(query)
+        conn.commit()
+    except:
+        print("Database table already exists.")
+
+
+createTable()
