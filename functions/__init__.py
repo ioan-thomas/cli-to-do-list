ca2275532d9d459cfe62a6ee911c2aeb4ad2c3f9
Ioan Thomas <ioanthomas21@gmail.com>
2022-12-20 13:56:43 +0000

    chore: comments

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,1 +1,2 @@
+# creates a module allowing me to import all of the below functions with ease.
 __all__ = ["addTask", "connectToDatabase", "createTable", "exitApp", "removeTask", "updateTask", "viewRemovedTasks", "viewTasks"]
