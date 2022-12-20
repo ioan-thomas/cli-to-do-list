6914461ca96892a7ff17f07cd9bf82a1294ea967
Ioan Thomas <ioanthomas21@gmail.com>
2022-12-20 12:45:10 +0000

    feat: update tasks in database

---
+++ functions/updateTask.py
@@ -0,0 +1,30 @@
+from functions.viewTasks import viewTasks
+
+def updateTask(conn, cursor):
+
+    # change way that viewTasks works - check for value in parameters, if there display the option for viewing more details
+    while True:
+        viewTasks(conn, cursor, False)
+
+        try: 
+            taskID = int(input("Enter the task ID you wish to update: "))
+            taskTitle = str(input("Enter the title of the task: "))
+            taskDetails = str(input("Enter the details for the task: "))
+
+            query = """UPDATE tasks SET Title = ?, Details = ? WHERE TaskID = ?"""
+
+            cursor.execute(query, (taskTitle, taskDetails, taskID))
+        
+        except KeyboardInterrupt:
+                print("\n\nGoodbye")
+                exit()
+        except:
+            print("Invalid input. Please check the task ID and try again.")
+        else:
+            conn.commit()
+            print(f"The task with the ID {taskID} has been updated.")
+            
+            userChoiceToExit = str(input("Do you wish to update another task? (Y/N): ")).upper()
+
+            if userChoiceToExit == 'N':
+                return
