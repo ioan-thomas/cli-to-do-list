7b4d38c59d5f62131f718200979693496f40956c
Ioan Thomas <ioanthomas21@gmail.com>
2023-01-06 10:16:49 +0000

    fix: user_choice wrong type

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from functions import *
 
 # creates a tuple with the connect and cursor objects as values - returned from the function. The tuple values reference the tasks.db passed in as a parameter.
-db =connectToDatabase('./tasks.db')
+db = connectToDatabase('./tasks.db')
 
 def main():
     # this function presents the user with options, asks user for input and runs the relevant function associated with that output.
@@ -25,25 +25,25 @@ def main():
                 print('Please enter a number between 1-7')
 
             # runs the relevant function associated with the user's output before beginning the loop again until the user either uses the exitApp function or they use the KeyboardInterrupt error to exit the app.
-            if user_choice == "1":
+            if user_choice == 1:
                 # runs the function and destructures the db tuple into the connect and cursor objects.
                 addTask(*db) # imported from functions/addTask.py
-            if user_choice == "2":
+            if user_choice == 2:
                 # runs the function and destructures the db tuple into the connect and cursor objects. The True parameter presents the user with additional functionality in the viewTasks function.
                 viewTasks(*db, True) # imported from functions/viewTasks.py
-            if user_choice == "3":
+            if user_choice == 3:
                 # runs the function and destructures the db tuple into the connect and cursor objects.
                 removeTask(*db) # imported from functions/removeTask.py
-            if user_choice == "4":
+            if user_choice == 4:
                 # runs the function and destructures the db tuple into the connect and cursor objects.
                 viewRemovedTasks(*db) # imported from functions/viewRemovedTasks.py
-            if user_choice == "5":
+            if user_choice == 5:
                 # runs the function function to delete a specified task and destructures the db tuple into the connect and cursor object.
                 deleteTask(*db)
-            if user_choice == "6":
+            if user_choice == 6:
                 # runs the function and destructures the db tuple into the connect and cursor objects.
                 updateTask(*db) # imported from functions/updateTask.py
-            if user_choice == "7":
+            if user_choice == 7:
                 # runs the below function to exit the app.
                 exitApp() # imported from functions/exitApp.py
         # if there is a KeyboardInterrupt error, the exitApp function is ran to exit the application.
