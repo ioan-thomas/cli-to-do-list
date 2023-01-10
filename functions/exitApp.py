a8203425c9645daf247253d4ed90fe061047354c
Ioan Thomas <ioanthomas21@gmail.com>
2023-01-10 17:53:15 +0000

    fix: quit function now used instead of exit

--- functions/exitApp.py
+++ functions/exitApp.py
@@ -2,4 +2,4 @@ def exitApp():
     # this app displays a goodbye message to the user before exitting the app.
     print("\n\nGoobye")
     # the Python function exit() is run to exit out of the application.
-    exit()
+    quit()
